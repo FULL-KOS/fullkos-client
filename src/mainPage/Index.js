9d45ed1b61162b50113ea2c1190511702de217c8
zigimi <chju0905@pusan.ac.kr>
2024-08-04 17:29:23 +0900

    feat | develop userContentsPage frame

--- src/mainPage/Index.js
+++ src/mainPage/Index.js
@@ -3,11 +3,11 @@ import KomPASS from '../KomPASS.png';
 
 function Index() {
     return (
-        <div>
-            <div>
+        <div className="MainPage">
+            <div className="MainPage">
                 <img src={KomPASS} width='60%'></img>
             </div>
-            <div className='MainInfo'>
+            <div className='MainInfo MainPage'>
                 어려운 주식투자 
                 <br/>
                 종목 분석을 쉽고 간편하게
