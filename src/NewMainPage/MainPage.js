fb8ee027bbf91d70239b7c867379eb84fef4419b
Bongmin Kang <kbm4250@naver.com>
2024-08-06 01:20:09 +0900

    feat | volume chart

--- src/NewMainPage/MainPage.js
+++ src/NewMainPage/MainPage.js
@@ -2,12 +2,15 @@ import CategoryMove from "../userReportPage/CategotyMove";
 import Portfolio from "../userReportPage/Portfolio";
 import styles from "./NewMainPage.module.css";
 import Upsurge from "./Upsurge/Upsurge";
+import Volume from "./Volume/Volume";
 
 function MainPage() {
   return (
     <div className={styles.container}>
       <div className={styles["grid-item"]}>carousel</div>
-      <div className={styles["grid-item"]}>mainleft</div>
+      <div className={styles["grid-item"]}>
+        <Volume />
+      </div>
       <div className={styles["grid-item"]}>
         <Upsurge />
       </div>
---
+++ src/NewMainPage/Volume/Volume.js
@@ -0,0 +1,98 @@
+import { Bar } from "react-chartjs-2";
+import styles from "./Volume.module.css";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+} from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  ChartDataLabels
+);
+Chart.defaults.font.family = "MALANGMALANG";
+
+function Volume() {
+  const data = {
+    labels: [
+      "삼성전자",
+      "현대자동차",
+      "동서D&S",
+      "SK하이닉스",
+      "한미반도체",
+      "신라",
+      "제우스",
+    ],
+    datasets: [
+      {
+        type: "bar",
+        backgroundColor: "#222222",
+        borderRadius: 2,
+        data: [1400000, 1220008, 1000010, 959000, 650000, 240000, 123000],
+        datalabels: {
+          align: "end",
+          anchor: "end",
+        },
+      },
+    ],
+  };
+
+  const options = {
+    indexAxis: "y",
+    scales: {
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        display: false,
+      },
+      tooltip: {
+        boxWidth: 15,
+      },
+      datalabels: {
+        formatter: function (value, context) {
+          // var idx = context.dataIndex;
+
+          return value;
+        },
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: {
+      duration: 2000,
+    },
+  };
+
+  return (
+    <div className={styles.volumeBox}>
+      <br />
+      최근 7일간 주요주주 거래량 순위
+      <div className={styles.chart}>
+        <Bar
+          type="bar"
+          data={data}
+          options={options}
+          plugins={ChartDataLabels}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Volume;
