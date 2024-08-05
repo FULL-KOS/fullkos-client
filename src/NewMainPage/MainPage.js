cf8147d1319b1b0f037e720cf81878b62ebf86f5
Bongmin Kang <kbm4250@naver.com>
2024-08-06 00:05:47 +0900

    feat | Impl newMainPage

--- src/NewMainPage/MainPage.js
+++ src/NewMainPage/MainPage.js
@@ -1,7 +1,15 @@
 import styles from "./NewMainPage.module.css";
 
 function MainPage() {
-  return <div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaatest</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles["grid-item"]}>carousel</div>
+      <div className={styles["grid-item"]}>mainleft</div>
+      <div className={styles["grid-item"]}>maincenter</div>
+      <div className={styles["grid-item"]}>mainright</div>
+      <div className={styles["grid-item"]}>portfolio</div>
+    </div>
+  );
 }
 
 export default MainPage;
